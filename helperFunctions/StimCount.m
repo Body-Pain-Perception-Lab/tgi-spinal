56f44f6219ca3581a7b61454e4f47df49ed9096f
amitchell12 <agmitchell@cfin.au.dk>
2022-03-08 12:15:06 +0100

    adding new StimCount function

---
+++ helperFunctions/StimCount.m
@@ -0,0 +1,11 @@
+%function StimCount()
+%% Stimulation count down
+% A G Mitchell - 07.03.2022
+% Last edited - 08.03.2022
+% 
+% small function that counts down x number of seconds (defined in
+% VAS_loadParams.m) before another screen is presented
+
+% Other functions needed:
+
+%end
