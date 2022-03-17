cd6a0d6bf431969c816f3314568afcea2685c102
amitchell12 <agmitchell@cfin.au.dk>
2022-03-17 10:44:45 +0100

    creating function that allows for counterbalancing

---
+++ pseudo_thermodes.m
@@ -0,0 +1,6 @@
+%% Pseudorandomise thermodes
+% A function to ensure effective counterbalancing of thermode location for
+% the spinal TGI study
+% Created by A.G. Mitchell on 17.03.2022
+% Last edited:
+
