255dc587fdae37bb2bf219395eb0692966a1ab7b
amitchell12 <agmitchell@cfin.au.dk>
2022-04-06 14:50:19 +0200

    coded file loadings - organised by type

--- analysis/tgispine_analysis.R
+++ analysis/tgispine_analysis.R
@@ -8,15 +8,33 @@ setwd(datPath)
 # load in all csv files
 filenames <- dir(datPath, recursive = TRUE, full.names = FALSE, pattern = '.csv')
 # empty dataframes for data
-df_trials <- read.csv(text='procedure,trial_type,arm,condition,dermatome,cold_probe,trial_tot')
-df_VAS <- read.csv(text='VASburning,VASwarm,VAScold,trial_tot')
-df_RT <- read.csv(text='RTburning,RTwarm,RTcold,trial_tot')
+df_trials <- read.csv(text='procedure,trial_type,arm,condition,dermatome,cold_probe,trial_tot,ID,manipulation')
+df_VAS <- read.csv(text='VASburning,VASwarm,VAScold,trial_tot,ID,manipulation')
+df_RT <- read.csv(text='RTburning,RTwarm,RTcold,trial_tot,ID,manipulation')
 
 ## need to find a way to index control and tgi files easily
 # trial files
 for (file in filenames){
-  if (isTRUE(substr(basename(file), 8, 8)=="p")){
-  #tmp <- 
+  if (isTRUE(substr(basename(file), 22, 22)=="t")){
+    tmp <- read.csv(file)
+    tmp$ID <- substr(basename(file), 1, 4)
+    tmp$manipulation <- substr(basename(file), 8, 10)
+    df_trials <- rbind(df_trials, tmp)
+  }
+  if (isTRUE(substr(basename(file), 22, 29)=="Response")){
+    tmp <- read.csv(file, header = FALSE)
+    colnames(tmp) <- c('VASburning','VASwarm','VAScold','trial_tot')
+    tmp$ID <- substr(basename(file), 1, 4)
+    tmp$manipulation <- substr(basename(file), 8, 10)
+    df_VAS <- rbind(df_VAS, tmp)
+  }
+  if (isTRUE(substr(basename(file), 22, 26)=="RespT")){
+    tmp <- read.csv(file, header = FALSE)
+    colnames(tmp) <- c('RTburning','RTwarm','RTcold','trial_tot')
+    tmp$ID <- substr(basename(file), 1, 4)
+    tmp$manipulation <- substr(basename(file), 8, 10)
+    df_RT <- rbind(df_RT, tmp)
+  }
 }
 
 # tgi files
