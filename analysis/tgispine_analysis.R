7aa1b54e29e4e1ff687e93ff444f0908992a4c56
amitchell12 <agmitchell@cfin.au.dk>
2022-04-11 17:32:35 +0200

    creating plots for pilot data

--- analysis/tgispine_analysis.R
+++ analysis/tgispine_analysis.R
@@ -5,7 +5,8 @@
 ##### Loading libraries & data #####
 library(dplyr)
 library(ggplot2)
-library("RColorBrewer")
+library(RColorBrewer)
+library(ggpubr)
 
 # paths
 datPath <- '/Users/au706616/Documents/Experiments/SPINALTGI/Raw/'
@@ -79,6 +80,8 @@ names(df_cold)[12] <- 'rating'
 df_plot <- rbind(df_burn, df_warm, df_cold)
 df_plot$VAS <- factor(df_plot$VAS, 
                              levels = c("cold", "warm", "burn"))
+df_plot$condition <- factor(df_plot$condition, 
+                      labels = c("ACR", "WIN"))
 
 # Define colors
 reds <- brewer.pal(6, "Reds")
@@ -96,44 +99,91 @@ colours <- c(caudal, rostral, distal, proximal)
 # isolate just my data (for now)
 AGM <- df_plot[df_plot$ID == '0001' ,]
 CEA <- df_plot[df_plot$ID == 'C001' ,]
+DE <- df_plot[df_plot$ID == 'D001' ,]
 
 # AGM - TGI
-ggplot(AGM[AGM$manipulation=='TGI' ,], aes(group = trial_n, colour = cold_probe)) +
-  geom_point(aes(condition, rating), position = position_dodge(.2)) +
+P1TGI <- ggplot(AGM[AGM$manipulation=='TGI' ,], aes(group = trial_n, colour = cold_probe)) +
+  geom_point(aes(condition, rating), size = 1.5, position = position_dodge(.2)) +
   facet_wrap(~VAS) +
   scale_colour_manual(values = colours) +
+  lims(y = c(0, 100)) +
   theme_classic() +
-  labs(title = 'TGI') +
-  theme(legend.position = 'bottom',
+  labs(title = 'TGI', x = 'Condition', y = '') +
+  theme(legend.position = 'none',
         legend.title = element_blank())
+
 # AGM - nonTGI
-ggplot(AGM[AGM$manipulation=='CNT' ,], aes(group = trial_n, colour = cold_probe)) +
-  geom_point(aes(condition, rating), position = position_dodge(.2)) +
+P1NTGI <- ggplot(AGM[AGM$manipulation=='CNT' ,], aes(group = trial_n, colour = cold_probe)) +
+  geom_point(aes(condition, rating), size = 1.5, position = position_dodge(.2)) +
   facet_wrap(~VAS) +
   scale_colour_manual(values = colours) +
   theme_classic() +
-  labs(title = 'Non TGI') +
+  lims(y = c(0, 100)) +
+  labs(title = 'Non TGI', x = 'Condition', y = 'VAS Rating') +
   theme(legend.position = 'bottom',
         legend.title = element_blank())
 
+P1 <- ggarrange(P1NTGI, P1TGI,
+                ncol = 1, nrow = 2,
+                common.legend = TRUE,
+                legend = 'bottom')
+ggsave('P1plot.png', P1, device = NULL, path = datPath, width = 5, height = 8)
+
 # CEA - TGI
-ggplot(CEA[CEA$manipulation=='TGI' ,], aes(group = trial_n, colour = cold_probe)) +
-  geom_point(aes(condition, rating), position = position_dodge(.2)) +
+P2TGI <- ggplot(CEA[CEA$manipulation=='TGI' ,], aes(group = trial_n, colour = cold_probe)) +
+  geom_point(aes(condition, rating), size = 1.5, position = position_dodge(.2)) +
   facet_wrap(~VAS) +
   scale_colour_manual(values = colours) +
+  lims(y = c(0, 100)) +
   theme_classic() +
-  labs(title = 'TGI') +
-  theme(legend.position = 'bottom',
+  labs(title = 'TGI', x = 'Condition', y = '') +
+  theme(legend.position = 'none',
         legend.title = element_blank())
+
 # CEA - nonTGI
-ggplot(CEA[CEA$manipulation=='CNT' ,], aes(group = trial_n, colour = cold_probe)) +
-  geom_point(aes(condition, rating), position = position_dodge(.2)) +
+P2NTGI <- ggplot(CEA[CEA$manipulation=='CNT' ,], aes(group = trial_n, colour = cold_probe)) +
+  geom_point(aes(condition, rating), size = 1.5, position = position_dodge(.2)) +
   facet_wrap(~VAS) +
   scale_colour_manual(values = colours) +
   theme_classic() +
-  labs(title = 'Non TGI') +
+  lims(y = c(0, 100)) +
+  labs(title = 'Non TGI', x = 'Condition', y = 'VAS Rating') +
   theme(legend.position = 'bottom',
         legend.title = element_blank())
+
+P2 <- ggarrange(P2NTGI, P2TGI,
+                ncol = 1, nrow = 2,
+                common.legend = TRUE,
+                legend = 'bottom')
+ggsave('P2plot.png', P2, device = NULL, path = datPath, width = 5, height = 8)
+
+# DE - TGI
+P3TGI <- ggplot(DE[DE$manipulation=='TGI' ,], aes(group = trial_n, colour = cold_probe)) +
+  geom_point(aes(condition, rating), size = 1.5, position = position_dodge(.2)) +
+  facet_wrap(~VAS) +
+  scale_colour_manual(values = colours) +
+  lims(y = c(0, 100)) +
+  theme_classic() +
+  labs(title = 'TGI', x = 'Condition', y = '') +
+  theme(legend.position = 'none',
+        legend.title = element_blank())
+
+# CEA - nonTGI
+P3NTGI <- ggplot(DE[DE$manipulation=='CNT' ,], aes(group = trial_n, colour = cold_probe)) +
+  geom_point(aes(condition, rating), size = 1.5, position = position_dodge(.2)) +
+  facet_wrap(~VAS) +
+  scale_colour_manual(values = colours) +
+  theme_classic() +
+  lims(y = c(0, 100)) +
+  labs(title = 'Non TGI', x = 'Condition', y = 'VAS Rating') +
+  theme(legend.position = 'bottom',
+        legend.title = element_blank())
+
+P3 <- ggarrange(P3NTGI, P3TGI,
+                ncol = 1, nrow = 2,
+                common.legend = TRUE,
+                legend = 'bottom')
+ggsave('P3plot.png', P3, device = NULL, path = datPath, width = 5, height = 8)
 
 
 ##### Summary statistics & plots #####
@@ -144,40 +194,77 @@ VASresponse <- aggregate(rating~ID*VAS*manipulation*condition*cold_probe*dermato
 # H1: the effect of dermatome
 VAS_H1 <- aggregate(rating~ID*VAS*manipulation*condition, 
                     mean, data = VASresponse)
+VAS_H1_SD <- aggregate(rating~ID*VAS*manipulation*condition, 
+                       sd, data = VASresponse)
+names(VAS_H1_SD)[5] <- 'SD'
+VAS_H1 <- merge(VAS_H1, VAS_H1_SD)
+
 # plotting - TGI
-ggplot(VAS_H1[VAS_H1$manipulation=='TGI' ,], 
-       aes(condition, rating, group = ID, colour = ID)) +
-  geom_point(position = position_dodge(.3)) +
+H1TGI <- ggplot(VAS_H1[VAS_H1$manipulation=='TGI' ,], 
+       aes(condition, rating, group = ID, colour = VAS)) +
+  geom_point(size = 1, position = position_dodge(.3)) +
+  geom_errorbar(aes(ymin=rating-SD, ymax=rating+SD), width=.2,
+                position=position_dodge(.3)) +
   facet_wrap(~VAS) +
+  scale_color_manual(values = c(blues[4],reds[4],purps[5])) +
   labs(title = 'TGI') +
-  theme_classic()
+  theme_classic() + 
+  theme(legend.position = 'none')
+
+add_summary(H1TGI, 'mean',
+            color = 'grey',
+            shape = 21,
+            size = .5)
+
+
 # non TGI
-ggplot(VAS_H1[VAS_H1$manipulation=='CNT' ,], 
-       aes(condition, rating, group = ID, colour = ID)) +
-  geom_point(position = position_dodge(.3)) +
+H1NTGI <- ggplot(VAS_H1[VAS_H1$manipulation=='CNT' ,], 
+       aes(condition, rating, group = ID, colour = VAS)) +
+  geom_point(size = 1, position = position_dodge(.3)) +
   facet_wrap(~VAS) +
+  geom_errorbar(aes(ymin=rating-SD, ymax=rating+SD), width=.2,
+                  position=position_dodge(.3)) +
+  scale_color_manual(values = c(blues[4],reds[4],purps[5])) +
   labs(title = 'Non TGI') +
-  theme_classic()
+  theme_classic() + 
+  theme(legend.position = 'none')
+
+add_summary(H1NTGI, 'mean',
+            color = 'grey',
+            shape = 21,
+            size = .5)
    
 # H2: across dermatome
-VAS_H2 <- VASresponse[VASresponse$condition == 'across' ,]
+VASSD <- aggregate(rating~ID*VAS*manipulation*condition*cold_probe*dermatome, 
+                         sd, data = df_plot)
+names(VASSD)[7] <- 'SD'
+VASresponse <- merge(VASresponse, VASSD)
+
+
+VAS_H2 <- VASresponse[VASresponse$condition == 'ACR' ,]
 # plotting - TGI
 ggplot(VAS_H2[VAS_H2$manipulation=='TGI' ,], 
-       aes(cold_probe, rating, group = ID, colour = ID)) +
+       aes(cold_probe, rating, group = ID, colour = VAS)) +
   geom_point(position = position_dodge(.3)) +
+  geom_errorbar(aes(ymin=rating-SD, ymax=rating+SD), width=.2,
+                position=position_dodge(.3)) +
   facet_wrap(~VAS) +
+  scale_color_manual(values = c(blues[4],reds[4],purps[5])) +
   labs(title = 'TGI') +
-  theme_classic()
+  theme_classic() +
+  theme(legend.position = 'none')
 # non TGI
 ggplot(VAS_H2[VAS_H2$manipulation=='CNT' ,], 
-       aes(cold_probe, rating, group = ID, colour = ID)) +
+       aes(cold_probe, rating, group = ID, colour = VAS)) +
   geom_point(position = position_dodge(.3)) +
   facet_wrap(~VAS) +
+  scale_color_manual(values = c(blues[4],reds[4],purps[5])) +
   labs(title = 'Non TGI') +
-  theme_classic()
+  theme_classic() +
+  theme(legend.position = 'none')
 
 # H3: within dermatome
-VAS_H3 <- VASresponse[VASresponse$condition == 'within' ,]
+VAS_H3 <- VASresponse[VASresponse$condition == 'WIN' ,]
 # plotting - TGI
 ggplot(VAS_H3[VAS_H3$manipulation=='TGI' ,], 
        aes(cold_probe, rating, group = ID, colour = ID)) +
