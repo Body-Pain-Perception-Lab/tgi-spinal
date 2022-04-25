36485a2ac1b0ad62c07b711a196cc91084397599
amitchell12 <agmitchell@cfin.au.dk>
2022-04-25 15:19:45 +0200

    making basic audio script

--- helperFunctions/audioPlay.m
+++ helperFunctions/audioPlay.m
@@ -1,29 +1,40 @@
 %function [] = audioPlay(scr, vars)
 %% PsychPortAudio function
-% Plays a simple tone for 200ms through psychotoolbox
-% will need ptgConfig and a paramater script to run as well
+% Plays a simple tone for 250ms through psychotoolbox
+% Uses code from ptbConfig (with an audio set up, where vars.control.audio
+% (in VAS_loadParams.m) needs to be set to 1, 
+% to make sure audio features are set up)
+% 
 % AG. Mitchell: 22.04.22
 
 %% Audio parameters
 InitializePsychSound(1);
 % Number of channels and Frequency of the sound
 nrchannels = 2;
-freq = 48000;
+sampRate = 48000;
 % How many times to we wish to play the sound
 repetitions = 1;
-% Length of the beep
-beepLengthSecs = 1;
-% Length of the pause between beeps
-beepPauseTime = 1;
-% Start immediately (0 = immediately)
-startCue = 0;
-
+beepLengthSecs = 0.25;
+beepPauseTime = 0.5; % Length of the pause between beeps
+startCue = 0; %Start immediately (0 = immediately)
 % Should we wait for the device to really start (1 = yes)
-% INFO: See help PsychPortAudio
 waitForDeviceStart = 1;
 
-pa = PsychPortAudio('Open');
+% open PTB audio
+pah = PsychPortAudio('Open', [], 1, 1, sampRate, nrchannels);
 
+% Sound settings
+PsychPortAudio('Volume', pah, 0.5);
+myBeep = MakeBeep(450, beepLengthSecs, sampRate); % Make a beep 
+
+% Fill the audio playback buffer with the audio data, doubled for stereo
+% presentation
+PsychPortAudio('FillBuffer', pah, [myBeep; myBeep]);
+
+% Start audio playback #1
+PsychPortAudio('Start', pah, repetitions, startCue, waitForDeviceStart);
+WaitSecs(beepPauseTime)
+PsychPortAudio('Stop', pah);
 
 
 
